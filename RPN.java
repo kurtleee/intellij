b5352592ff14419294c6c7f5a8c948860acd4ef4
Krut Lee <59387051+kurtleee@users.noreply.github.com>
2022-11-13 13:01:45 +0800

    interface

--- RPN.java
+++ RPN.java
@@ -1,14 +1,22 @@
 import java.util.Deque;
 import java.util.LinkedList;
 
-public class RPN {
+interface inter {
+    int evalRPN(String[] tokens);
+
+    boolean isNumber(String token);
+
+}
+
+
+public class RPN implements inter{
     public static void main(String[] args) {
         String[] tokens = new String[]{"1", "3", "+"};
         RPN o1  = new RPN();
         System.out.print(o1.evalRPN(tokens));
     }
 
-    public int evalRPN(String[] tokens) {
+    public int evalRPN (String[] tokens) {
         Deque<Integer> stack = new LinkedList<>();
 
         for (String token : tokens) {
