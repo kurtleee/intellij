7d7fc005a7a4405d4a719d2015e419b8d1f2d561
Krut Lee <59387051+kurtleee@users.noreply.github.com>
2022-11-13 12:20:34 +0800

    add RPN

---
+++ RPN.java
@@ -0,0 +1,34 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class RPN {
+    public static void main(String[] args) {
+        String[] tokens = new String[]{"1", "3", "+"};
+        RPN o1  = new RPN();
+        System.out.print(o1.evalRPN(tokens));
+    }
+
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new LinkedList<>();
+
+        for (String token : tokens) {
+            if (isNumber(token)) {
+                stack.push(Integer.valueOf(token));
+            } else {
+                int t2 = stack.pop();
+                int t1 = stack.pop();
+                switch (token) {
+                    case "+" -> stack.push(t1 + t2);
+                    case "-" -> stack.push(t1 - t2);
+                    case "*" -> stack.push(t1 * t2);
+                    case "/" -> stack.push(t1 / t2);
+                }
+            }
+        }
+        return stack.pop();
+    }
+
+    public boolean isNumber(String token) {
+        return !("-".equals(token) || "+".equals(token) || "*".equals(token) || "/".equals(token));
+    }
+}
