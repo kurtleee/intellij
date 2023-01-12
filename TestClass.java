16c13f8428f1e03c6c5e9948b1723ad763750b15
Krut Lee <59387051+kurtleee@users.noreply.github.com>
2023-01-13 02:17:50 +0800

    minor changes to TestClass.java

---
+++ TestClass.java
@@ -0,0 +1,16 @@
+public class TestClass {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        System.out.println(s);
+
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s); 
+        System.out.println(s);
+        System.out.println(s);
+    }
+
+
+}
+
+
