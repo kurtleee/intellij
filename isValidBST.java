268a85135be23a17e449b91555ea28bec570fd18
Krut Lee <59387051+kurtleee@users.noreply.github.com>
2023-03-16 21:36:22 +0800

    update

--- isValidBST.java
+++ isValidBST.java
@@ -1,5 +1,6 @@
 import com.sun.source.tree.Tree;
-
+/*Created by @Kurt LEE On 3/16/23 9:33 PM.*/
+import java.util.ArrayList;
 import java.util.List;
 
 public class isValidBST {
@@ -8,12 +9,25 @@ public class isValidBST {
     }
 
     public boolean isValid(TreeNode root) {
-
+        List<Integer> values = new ArrayList<>();
+        inOrderTraversal(root, values);
+        for (int i = 0; i <= values.size() - 1; ++i) {
+            if (values.get(i) >= values.get(i - 1)) {
+                return false;
+            }
+        }
 
         return true;
     }
 
+    // 使用中序遍历，输出有顺序的values；若给定二叉树并非BST，则会在isValid 中判断出来。
     private void inOrderTraversal(TreeNode node, List<Integer> values) {
+        if (node == null) {
+            return ;
+        }
 
+        inOrderTraversal(node.left, values);
+        values.add(node.val);
+        inOrderTraversal(node.right, values);
     }
 }
