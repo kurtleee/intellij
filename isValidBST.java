3ac0f1c96d83a29ea521f6386066a37259ae4340
Krut Lee <59387051+kurtleee@users.noreply.github.com>
2023-03-16 21:30:01 +0800

    update

---
+++ isValidBST.java
@@ -0,0 +1,19 @@
+import com.sun.source.tree.Tree;
+
+import java.util.List;
+
+public class isValidBST {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isValid(TreeNode root) {
+
+
+        return true;
+    }
+
+    private void inOrderTraversal(TreeNode node, List<Integer> values) {
+
+    }
+}
