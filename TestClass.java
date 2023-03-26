99d62b7da5816d644263b81beba80b0cccaf1077
Krut Lee <59387051+kurtleee@users.noreply.github.com>
2023-03-27 03:46:50 +0800

    updated

--- TestClass.java
+++ TestClass.java
@@ -1,28 +1,9 @@
-import java.util.*;
-
 public class TestClass {
     public static void main(String[] args) {
+        char[] s = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+
 
     }
 
-    public int maxFrequency(int[] nums, int k) {
-        Map<Integer, Integer> freq = new HashMap<>();
-        for (int num : nums) {
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
-        }
-
-        int maxFreq = 0;
-        List<Integer> sortedFreq = new ArrayList<>(freq.values());
-        Collections.sort(sortedFreq, Collections.reverseOrder());
-
-        for (int i = 0; i < sortedFreq.size(); i++) {
-            int f = sortedFreq.get(i);
-            int inc = (i + 1 < sortedFreq.size()) ? (freq.size() * (f - sortedFreq.get(i+1))) : k / f;
-            if (inc > k) break;
-            k -= inc;
-            maxFreq = f + inc / freq.size();
-        }
-
-        return maxFreq;
-    }
+    private 
 }
