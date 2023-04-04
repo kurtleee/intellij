6a37bb093b44ef581aff4939c86df8aa01c495f2
Krut Lee <59387051+kurtleee@users.noreply.github.com>
2023-04-05 02:04:35 +0800

    updated

--- PalindromePartitioning.java
+++ PalindromePartitioning.java
@@ -1,8 +1,47 @@
 /*Created by @Kurt LEE. Last Modified on 4/5/23, 12:54 AM.*/
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PalindromePartitioning {
     public static void main(String[] args) {
 
+    }
+
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        List<String> path = new ArrayList<>();
+        // 预处理回文串
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for (int i = 0; i < s.length(); ++i) {
+            for (int j = 0; j <= i; ++j) {
+                if (s.charAt(i) == s.charAt(j) && (i - j <= 2 || dp[j + 1][i - 1])) {
+                    dp[j][i] = true;
+                }
+            }
+        }
+        // 回溯搜索所有合法的划分方案
+        backtrack(s, 0, res, path, dp);
+        return res;
+    }
+
+    private void backtrack(String s, int start, List<List<String>> res, List<String> path, boolean[][] dp) {
+        if (start == s.length()) {
+            // 当搜索到字符串末尾时，将当前路径加入到结果中
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = start; i < s.length(); ++i) {
+            if (dp[start][i]) {
+                // 当[start, i]是回文子串时，将其加入当前路径中
+                path.add(s.substring(start, i + 1));
+                // 继续深度搜索后续字符
+                backtrack(s, i + 1, res, path, dp);
+                // 回溯到上一个状态
+                path.remove(path.size() - 1);
+            }
+        }
     }
 
 
--- TopK.java
+++ TopK.java
@@ -1,18 +1,28 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class TopK {
-    public void TopKElements (int[] arr, int k, int n) {
-        // Create a HashMap mp to store the key & value pair.
-        Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
-        for (int num : arr) {
-            mp.put(0, mp.getOrDefault(num, 0) + 1);
+    public int[] topKFrequent(int[] nums, int k) {
+        // 1. 统计每个数字的出现频率
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
         }
 
-        // Create a list based on the HashMap mp we just created.
-        List<Map.Entry<Integer, Integer> > list = new ArrayList<>();
+        // 2. 维护一个小根堆，保存当前出现频率最高的k个元素
+        PriorityQueue<Integer> heap = new PriorityQueue<>((o1, o2) -> freq.get(o1) - freq.get(o2));
+        for (int num : freq.keySet()) {
+            heap.offer(num);
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
 
+        // 3. 转换结果并返回
+        int[] res = new int[k];
+        int i = 0;
+        for (int num : heap) {
+            res[i++] = num;
+        }
+        return res;
     }
 }
